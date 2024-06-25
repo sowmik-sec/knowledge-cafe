34af472f5a0f8ca2884f3ae0204388cb7b050855
sowmik-sec <sowmik.sec@gmail.com>
2024-06-25 13:01:59 +0600

    create bookmarks component

---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import Bookmark from "../Bookmark/Bookmark";
+
+function Bookmarks({ bookmarks, readingTime }) {
+  return (
+    <div className="md:w-1/3 bg-gray-300 ml-4 mt-2 pt-4">
+      <div>
+        <h3 className="text-3xl">{readingTime}</h3>
+      </div>
+      <h2 className="text-2xl text-center ">
+        Bookmarked Blogs:{bookmarks.length}
+      </h2>
+      {bookmarks.map((bookmark, i) => (
+        <Bookmark key={i} bookmark={bookmark} readingTime={readingTime} />
+      ))}
+    </div>
+  );
+}
+
+Bookmarks.propTypes = {
+  bookmarks: PropTypes.array.isRequired,
+  readingTime: PropTypes.number.isRequired,
+};
+
+export default Bookmarks;
